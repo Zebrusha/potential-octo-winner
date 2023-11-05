fddef56c0c607cfa9fcf480c724cfdfd78b31012
Zebrusha <146824554+Zebrusha@users.noreply.github.com>
2023-11-05 16:26:35 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int cost = 13000;
-        int bonus = cost/20;
+        int bonus = cost / 20;
 
         System.out.println("Количетсво начисленных бонусов: " + bonus);
 
